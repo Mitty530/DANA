025942c6161a68d2fe8e14b8000a2eb8426a6708
Ousmane <ousmanehabi.168@gmail.com>
2024-11-16 06:45:51 +0000

    add new modification for the main page

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -10,58 +10,27 @@ function Header() {
                             <div className="row align-items-center justify-content-between">
                                 <div className="col-auto">
                                     <div className="header-logo">
-                                        <a href="index.html">
-                                            <img src="/assets/img/logo.svg" alt="Carva" /></a>
+                                        <a href="index.html" className="h4">
+                                            {/* <img src="/assets/img/logo.svg" alt="Carva" /> */}
+                                            DNA - Smart Parking
+                                            </a>
+
 
                                     </div>
                                 </div>
                                 <div className="col-auto ms-auto">
                                     <nav className="main-menu">
                                         <ul>
-                                            <li><a href="about.html">About</a></li>
-                                            <li><a href="ride.html">Ride</a></li>
-                                            <li><a href="drive.html">Drive</a></li>
-                                            <li className="menu-item-has-children">
-                                                <ul className="sub-menu">
-                                                    <li className="menu-item-has-children">
-                                                        <ul className="sub-menu">
-                                                            <li><a href="dashboard-overview.html">Dashboard Overview</a></li>
-                                                            <li><a href="dashboard-drivers.html">Dashboard Driver</a></li>
-                                                            <li><a href="dashboard-passengers.html">Dashboard Passengers</a></li>
-                                                            <li><a href="dashboard-payout.html">Dashboard Payout</a></li>
-                                                            <li><a href="dashboard-trips.html">Dashboard Trips</a></li>
-                                                            <li><a href="driver-overview.html">Driver Overview</a></li>
-                                                            <li><a href="driver-message.html">Driver Message</a></li>
-                                                            <li><a href="driver-trips.html">Driver Trips</a></li>
-                                                            <li><a href="driver-trip-details.html">Driver Trip Details</a></li>
-                                                            <li><a href="driver-settings.html">Driver Settings</a></li>
-                                                            <li><a href="driver-vehicle.html">Driver Vehicles</a></li>
-                                                            <li><a href="passenger-overview.html">Passenger Overview</a></li>
-                                                            <li><a href="passenger-message.html">Passenger Message</a></li>
-                                                            <li><a href="passenger-trips.html">Passenger Trips</a></li>
-                                                            <li><a href="passenger-trip-details.html">Passenger Trip Details</a></li>
-                                                        </ul>
-                                                    </li>
-                                                    <li><a href="blog-details.html">Blog Details</a></li>
-                                                    <li><a href="contact.html">Contact Us</a></li>
-                                                    <li><a href="faq.html">Faq Page</a></li>
-                                                    <li><a href="privacy.html">Privacy Policy</a></li>
-                                                    <li><a href="ride-on-web.html">Ride On Web</a></li>
-                                                    <li><a href="rider-sign-up.html">Rider Sign Up</a></li>
-                                                    <li><a href="driver-sign-up.html">Driver Sign Up</a></li>
-                                                    <li><a href="terms.html">Terms & Conditions</a></li>
-                                                    <li><a href="sitemap.html">Sitemap</a></li>
-                                                </ul>
-                                            </li>
-                                            <li><a href="event.html">Events</a></li>
-                                            <li><a href="blog.html">Blog</a></li>
-                                            <li><a href="sign-in.html">Log In</a></li>
+                                            <li><a href="about.html">Guide</a></li>
+                                            <li><a href="ride.html">Download App</a></li>
+                                            <li><a href="drive.html">Features</a></li>
+                                            <li><a href="sign-in.html">Contact Us</a></li>
                                         </ul>
                                     </nav>
                                 </div>
                                 <div className="col-auto">
                                     <div className="header-button">
-                                        <a href="sign-up.html" className="ot-btn style2">Get Started</a>
+                                        <a href="sign-up.html" className="ot-btn style2">Log In / Sign Up</a>
                                         <button type="button" className="ot-menu-toggle d-inline-block d-lg-none"><i className="fal fa-bars"></i></button>
                                     </div>
                                 </div>
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,120 +1,144 @@
+
 function Home() {
     return (
-        <div className="hero-1 ot-hero-wrapper">
-        <div className="container">
-            <div className="hero-style1">
-                <div className="row align-items-center">
-                    <div className="col-xl-6">
-                        <h1 className="hero-title cd-headline rotate-1">
-                            Creative Technologies Provided in
-                            <span className="cd-words-wrapper text-theme">
-                                <b className="is-visible">Taxis</b>
-                                <b>Seats</b>
+        <>
+            <div className="hero-1 ot-hero-wrapper">
+                <div className="container">
+                    <div className="hero-style1">
+                        <h4 className="hero-title cd-headline rotate-1 text-center mt-4">
+                            Reserve Your Parking  <span className="cd-words-wrapper text-theme">
+                                <b className="is-visible">Now</b>
+                                <b className="is-hidden">Save</b>
                             </span>
-                        </h1>
-                        <p className="hero-text">Scelerisque est bibendum id sed quam nisl pretium. Felis, dolor congue laoreet a, nam et ac arcu.</p>
-                        <div className="brand-box">
-                            <h6 className="brand-box__title">Our Partners</h6>
-                            <div className="brand-box__logos">
-                                <img src="/assets/img/brand/brand_1_1.png" alt="Logo"  />
-                                <img src="/assets/img/brand/brand_1_2.png" alt="Logo" />
-                                <img src="/assets/img/brand/brand_1_3.png" alt="Logo" />
-                                <img src="/assets/img/brand/brand_1_4.png" alt="Logo" />
+                        </h4>
+                        <div className="row align-items-center mb-3">
+                            <div className="col-xl-8 offset-2 ">
+                                <form>
+                                    <div className="input-group">
+                                        <input type="text" className="form-control" name="destination" id="destination" placeholder="Address, Place, City" />
+                                        <button className="btn btn-primary" type="submit"><i className="fas fa-location-dot"></i>  Search </button>
+                                    </div>
+                                </form>
+
                             </div>
                         </div>
-                    </div>
-                    <div className="col-xl-5 offset-xl-1">
-                        <div className="combine-form">
-                            <div className="shape1">
-                                <img src="/assets/img/normal/dots_1.svg" alt="shape" />
+                        <div className="row align-items-center mb-3">
+                            <div className="col-xl-6 offset-5 mt-3">
+                                <button className="ot-btn style2">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pin-map-fill" viewBox="0 0 16 16">
+                                        <path fill-rule="evenodd" d="M3.1 11.2a.5.5 0 0 1 .4-.2H6a.5.5 0 0 1 0 1H3.75L1.5 15h13l-2.25-3H10a.5.5 0 0 1 0-1h2.5a.5.5 0 0 1 .4.2l3 4a.5.5 0 0 1-.4.8H.5a.5.5 0 0 1-.4-.8z" />
+                                        <path fill-rule="evenodd" d="M4 4a4 4 0 1 1 4.5 3.969V13.5a.5.5 0 0 1-1 0V7.97A4 4 0 0 1 4 3.999z" />
+                                    </svg> &nbsp;
+                                    Parking near to me
+                                </button>
                             </div>
-                            <nav>
-                                <div className="nav nav-tabs form-tab" role="tablist">
-                                    <button className="nav-link" id="nav-ride-tab" data-bs-toggle="tab" data-bs-target="#nav-ride" type="button" role="tab" aria-controls="nav-ride" aria-selected="false"><i className="fas fa-car"></i> Take a Ride</button>
-                                    <button className="nav-link active" id="nav-drive-tab" data-bs-toggle="tab" data-bs-target="#nav-drive" type="button" role="tab" aria-controls="nav-drive" aria-selected="true"><i className="far fa-steering-wheel"></i> Apply to Drive</button>
+
+                        </div>
+
+                    </div>
+                </div>
+                <div className="hero-img">
+                    {/* <img src="/assets/img/normal/hero_img_1.jpg" alt="Hero Image" /> */}
+                </div>
+                <div className="hero-shape1">
+                    <img src="/assets/img/normal/dots_2.svg" alt="dots" />
+                </div>
+                <div className="shape-group position-2">
+                    <div className="shape shape1"></div>
+                    <div className="shape shape2"></div>
+                    <div className="shape shape3"></div>
+                </div>
+            </div>
+            {/* section 2 */}
+            <section id="feature-icons-id" className="css-kh6mfs py-5 bg-light">
+                <div className="container">
+                    <h2 className="text-center fw-bold text-primary mb-5">How DNA - Smart Parking Works</h2>
+                    <div className="row text-center gy-4">
+                        <div className="col-md-4">
+                            <div className="p-4 ride-box rounded-10">
+                                <div className="mb-3 text-primary">
+                                    <i className="bi bi-search fs-1"></i>
                                 </div>
-                            </nav>
-                            <div className="tab-content">
-                                <div className="tab-pane fade" id="nav-ride" role="tabpanel" aria-labelledby="nav-ride-tab">
-                                    <form action="#" className="form1">
-                                        <h3 className="form-title">Get member exclusive rewards</h3>
-                                        <p className="form-text">Egestas sed vulputate eleifend ac adipiscing quisque. Hac vulputate integer sapien et.</p>
-                                        <div className="row">
-                                            <div className="form-group col-md-6">
-                                                <input type="text" class="form-control" name="firstName" id="firstName" placeholder="First Name" />
-                                            </div>
-                                            <div class="form-group col-md-6">
-                                                <input type="text" class="form-control" name="lastName" id="lastName" placeholder="Last Name" />
-                                            </div>
-                                            <div className="form-group col-12">
-                                                <input type="number" class="form-control" name="number" id="number" placeholder="Phone Number" />
-                                            </div>
-                                            <div class="form-group col-12">
-                                                <input type="email" class="form-control" name="email" id="email" placeholder="Email Address" />
-                                            </div>
-                                            <div class="form-group col-12">
-                                                <input type="text" className="form-control" name="location" id="location" placeholder="City" />
-                                            </div>
-                                            <div className="col-12 form-group mt-3">
-                                                <input type="checkbox" id="agree" />
-                                                <label for="agree">I agree to the <a href="terms.html">Terms and Conditions</a> and <a href="privacy.html">Privacy Policy</a></label>
-                                            </div>
-                                            <div className="form-btn col-12">
-                                                <button class="ot-btn">Sign up to Ride</button>
-                                            </div>
-                                        </div>
-                                        <p className="form-messages mb-0 mt-3"></p>
-                                    </form>
+                                <h3 className="fw-semibold">Explore</h3>
+                                <p className="text-muted">
+                                    Find and compare affordable parking locations across the Abu Dhabi .
+                                </p>
+                            </div>
+                        </div>
+                        <div className="col-md-4">
+                            <div className="p-4 ride-box rounded-10">
+                                <div className="mb-3 text-primary">
+                                    <i className="bi bi-credit-card fs-1"></i>
                                 </div>
-                                <div className="tab-pane fade show active" id="nav-drive" role="tabpanel" aria-labelledby="nav-drive-tab">
-                                    <form action="#" className="form1">
-                                        <h3 className="form-title">Start driving now and get paid</h3>
-                                        <p className="form-text">Egestas sed vulputate eleifend ac adipiscing quisque. Hac vulputate integer sapien et.</p>
-                                        <div className="row">
-                                            <div className="form-group col-md-6">
-                                                <input type="text" className="form-control" name="firstName" id="firstName2" placeholder="First Name" />
-                                            </div>
-                                            <div className="form-group col-md-6">
-                                                <input type="text" className="form-control" name="lastName" id="lastName2" placeholder="Last Name" />
-                                            </div>
-                                            <div className="form-group col-12">
-                                                <input type="number" className="form-control" name="number" id="number2" placeholder="Phone Number" />
-                                            </div>
-                                            <div className="form-group col-12">
-                                                <input type="email" className="form-control" name="email" id="email2" placeholder="Email Address" />
-                                            </div>
-                                            <div className="form-group col-12">
-                                                <input type="text" className="form-control" name="location" id="location2" placeholder="City" />
-                                            </div>
-                                            <div className="col-12 form-group mt-3">
-                                                <input type="checkbox" id="agree2" />
-                                                <label for="agree2">I agree to the <a href="terms.html">Terms and Conditions</a> and <a href="privacy.html">Privacy Policy</a></label>
-                                            </div>
-                                            <div className="form-btn col-12">
-                                                <button className="ot-btn">Become a Driver</button>
-                                            </div>
-                                        </div>
-                                        <p className="form-messages mb-0 mt-3"></p>
-                                    </form>
+                                <h3 className="fw-semibold">Reserve</h3>
+                                <p className="text-muted">
+                                    Securely book your spot and receive instant confirmation via email
+                                    or app.
+                                </p>
+                            </div>
+                        </div>
+                        <div className="col-md-4">
+                            <div className="p-4 ride-box rounded-10">
+                                <div className="mb-3 text-primary">
+                                    <i className="bi bi-car-front-fill fs-1"></i>
                                 </div>
+                                <h3 className="fw-semibold">Arrive</h3>
+                                <p className="text-muted">
+                                    Follow your instructions, park your car, and enjoy your time.
+                                </p>
                             </div>
                         </div>
                     </div>
                 </div>
+            </section>
+            {/* section 3 */}
+            <section className="space position-relative overflow-hidden">
+        <div className="container">
+            <div className="row space-bottom wow fadeInUp" data-wow-delay="0.1s">
+                <div className="col-xl-5 col-lg-6">
+                    <div className="img-box2">
+                        <div className="img1">
+                            <img src="/assets/img/normal/feature_1.jpg" alt="feature" />
+                        </div>
+                    </div>
+                </div>
+                <div className="offset-xl-1 col-lg-6">
+                    <div className="title-area mb-4">
+                        <span className="subtitle">Safety</span>
+                        <h2 className="sec-title">Our Safety Protocol</h2>
+                    </div>
+                    <p className="mb-30">Tristique tortor dui morbi eu. Nullam ornare lorem gravida amet lacus, ut. Eget aenean volutpat diam ullamcorper at nunc lorem gravida.</p>
+                    <a href="about.html" className="ot-btn">Learn More</a>
+                </div>
+            </div>
+            <div className="row flex-row-reverse space-extra-top wow fadeInUp" data-wow-delay="0.1s">
+                <div className="col-xl-5 col-lg-6 offset-xl-2">
+                    <div className="img-box3">
+                        <div className="img1">
+                            <img src="/assets/img/normal/feature_2.jpg" alt="feature" />
+                        </div>
+                        <div className="shape1">
+                            <img src="/assets/img/normal/dots_1.svg" alt="dot shape" />
+                        </div>
+                    </div>
+                </div>
+                <div className="col-xl-5 col-lg-6">
+                    <div className="title-area mb-4">
+                        <span className="subtitle">Vaccine access fund</span>
+                        <h2 className="sec-title">Support Our Vaccine <br /> Access Fund</h2>
+                    </div>
+                    <p className="mb-30">Vel enim tortor arcu mi ut aliquam ultrices. Nunc, sit turpis nisl cursus mauris pretium massa elit.</p>
+                        <a href="#" className="ot-btn" >Donate</a>
+                </div>
             </div>
         </div>
-        <div className="hero-img">
-            {/* <img src="/assets/img/normal/hero_img_1.jpg" alt="Hero Image" /> */}
-        </div>
-        <div className="hero-shape1">
-            <img src="/assets/img/normal/dots_2.svg" alt="dots" />
-        </div>
-        <div className="shape-group position-2">
+        <div className="shape-group position-3">
             <div className="shape shape1"></div>
             <div className="shape shape2"></div>
             <div className="shape shape3"></div>
         </div>
-    </div>
+    </section>
+        </>
     )
 }
 
